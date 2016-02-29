a0a92e953f0ed978ff19e633457479e5749d14e3
Beat Richartz <beat.richartz@gmail.com>
2016-02-29 23:03:32 +0000

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ParallelStream.Mixfile do
   def project do
     [
         app: :parallel_stream,
-        version: "1.0.2",
+        version: "1.0.3",
         elixir: "~> 1.1",
         deps: deps,
         package: package,
