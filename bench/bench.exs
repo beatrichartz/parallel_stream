c75c86876156af7b3aa9252388b3130771243a50
Beat Richartz <beat.richartz@gmail.com>
2016-04-16 11:11:24 +0100

    Cleaner benchmark

--- bench/bench.exs
+++ bench/bench.exs
@@ -3,6 +3,9 @@ defmodule Bench do
   Benchfella.start
 
   setup_all do
+    us = average_test_func_call_time
+    IO.write("Function call is taking ~#{us} microseconds\n\n")
+
     if System.get_env("PS_BENCH_OBSERVER") do
       :observer.start()
     end
@@ -12,17 +15,27 @@ defmodule Bench do
 
   bench "stream" do
     1..10000
-    |> Stream.map(fn _ ->
-      :erlang.md5(:crypto.strong_rand_bytes(32))
-    end)
+    |> Stream.map(&test_func/1)
     |> Stream.run
   end
 
   bench "parallel_stream" do
     1..10000
-    |> ParallelStream.map(fn _ ->
-      :erlang.md5(:crypto.strong_rand_bytes(32))
-    end)
+    |> ParallelStream.map(&test_func/1)
     |> Stream.run
+  end
+
+  defp test_func(_) do
+    test_func
+  end
+  defp test_func do
+    :crypto.strong_rand_bytes(600)
+  end
+
+  defp average_test_func_call_time do
+    (1..10000 |> Enum.reduce(0, fn _, acc ->
+      { us, _ } = :timer.tc(&test_func/0)
+      acc + us
+    end)) / 10000 |> Float.round(2)
   end
 end
